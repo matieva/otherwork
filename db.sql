d0e2453de32e4883b37974de1326a1359d5f48ab
matieva <110250324+matieva@users.noreply.github.com>
2022-08-02 14:25:44 +0200

    init

---
+++ db.sql
@@ -0,0 +1,16 @@
+-- SQL3 Basic Query
+
+
+-- Gjalt når vi tok SELECT DISTINCT
+--SQL Server Hash Match Aggregate operator is used to process the large tables that are not sorted using an index.
+
+
+-- common: SELECT, TOP, FROM, WHERE UNION
+-- less common: OFFSET EXCEPT, INTERSECT
+-- predicates and SARGability (finne for den måten den er lagd, uten å gi andre oppsett for å finne. richard like eksempel)
+-- useful functions
+-- joins
+-- proter indenting
+
+
+

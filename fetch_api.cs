9ae17a77d8f28be6dc88026f2b8aa8c52a414666
matieva <110250324+matieva@users.noreply.github.com>
2022-08-30 14:30:20 +0200

    i

--- fetch_api.cs
+++ fetch_api.cs
@@ -1,1 +1,18 @@
+using System.Text.Json;
 
+using var httpClient = new HttpClient();
+
+var url = "https://raw.githubusercontent.com/dotnet/core/master/release-notes/releases-index.json";
+var ts = await httpClient.GetStreamAsync(url);
+
+using var resp = await JsonDocument.ParseAsync(ts);
+
+var root = resp.RootElement.GetProperty("releases-index");
+
+var elems = root.EnumerateArray();
+
+while (elems.MoveNext())
+{
+    var node = elems.Current;
+    Console.WriteLine(node);
+}

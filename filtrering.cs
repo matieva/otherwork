fe5ab26e339e91e0147b34fa1436ab3c3e73b68f
matieva <110250324+matieva@users.noreply.github.com>
2022-09-06 15:17:43 +0200

    filt

--- filtrering.cs
+++ filtrering.cs
@@ -1,1 +1,5 @@
-
+// filtrere på relaterte objekter og objekter som overlapper hverandre.
+// spesifisere spørringer blir et et enkelt spørrespråk benyttet.
+// spørrespråket støtter vegobjekter. 
+// avanserte spørring når spørringer involverer flere objekttyper. 
+// 
